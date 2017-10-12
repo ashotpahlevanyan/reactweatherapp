e8ab3a0cc5b2e61892460bc25a2749b877a50a1d
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-10-13 02:06:47 +0400

    add course with debuggers

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,7 @@
 import React, {PropTypes} from "react";
 import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
+
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -19,13 +21,19 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
+    debugger;
     return(
        <div>
          <h1>Courses</h1>
+         {this.props.courses.map(this.courseRow)}
          <h2>Add Course</h2>
          <input
            type="text"
@@ -41,4 +49,19 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+// const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+// export default connectedStateAndProps(CoursePage);
+
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  debugger;
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
