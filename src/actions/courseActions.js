bec05e17c10f8c3141783346587dfd975a3ddf93
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-10-14 03:03:07 +0400

    added error handling and toastr

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import courseApi from '../api/mockCourseApi';
 import * as types from './actionTypes';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
@@ -31,6 +31,7 @@ export function saveCourse(course) {
     return courseApi.saveCourse(course).then(course => {
       course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw(error)
     });
   };
