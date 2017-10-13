6361282ee4905360d65fb66cdd80e76626b32564
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-10-13 13:17:34 +0400

    added mapDispatchToProps function

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -52,8 +52,8 @@ class CoursesPage extends React.Component {
 // export default connectedStateAndProps(CoursePage);
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -62,4 +62,10 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse : (course) => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
