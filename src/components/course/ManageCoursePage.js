2f3d65f8af3ebb25a10cb8e862c39b429a3d02d7
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-10-13 20:18:37 +0400

    before componentwillrecieveprops lifecycle function is added, author selection still not working

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -23,7 +23,7 @@ class ManageCoursePage extends React.Component {
     course[field] = event.target.value;
     return this.setState({course: course});
   }
-this
+
   saveCourse(event) {
     event.preventDefault();
     this.props.actions.saveCourse(this.state.course);
@@ -53,9 +53,20 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object.isRequired
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id !== id);
+  if(course) return course[0]; // since filter returns an array, have to grab the first element
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; // from the path /course/:id
+
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
 
+  if(courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
       value: author.id,
