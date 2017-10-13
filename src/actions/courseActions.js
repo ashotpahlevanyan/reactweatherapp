e5feba420323fe2b0a744232f1da80b8a1c7df61
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-10-13 18:30:15 +0400

    added create course success and update action creators

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,13 @@ export function loadCoursesSuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
 
 export function loadCourses() {
   return function(dispatch) {
@@ -12,6 +19,16 @@ export function loadCourses() {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
       throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course) : dispatch(createCourseSuccess(course)));
+    }).catch(error => {
+      throw(error)
     });
   };
 }
