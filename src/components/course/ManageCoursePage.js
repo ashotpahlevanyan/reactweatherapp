73b9e9e4dbde3d70e6903164d27ce3e69df68ad0
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-10-14 18:09:16 +0400

    testing reducers and actions

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -4,6 +4,7 @@ import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
+import {authorsFormattedForDropdown} from '../../selectors/authorSelectors';
 
 export class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -106,16 +107,10 @@ function mapStateToProps(state, ownProps) {
   if(courseId && state.courses.length > 0) {
     course = getCourseById(state.courses, courseId);
   }
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + ' ' + author.lastName
-    };
-  });
 
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
