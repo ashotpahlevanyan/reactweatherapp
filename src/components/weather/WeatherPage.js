bb73c83aea6ca84d92b5592ef609a59be14d8577
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2017-11-03 19:18:34 +0400

    working on css, almost done

--- src/components/weather/WeatherPage.js
+++ src/components/weather/WeatherPage.js
@@ -10,14 +10,56 @@ class WeatherPage extends React.Component {
   }
 
   render() {
-    const {weather} = this.props;
-
     return(
       <div>
         <h2>The weather from weather-js API</h2>
-        <WeatherList weather={weather} />
+        {/*<WeatherList weather={weather} />*/}
+        <div className="weatherWidget clearfix">
+          <div className="leftSide">
+            <div className="date">Friday,</div>
+            <div className="date">August 23 rd</div>
+            <span className="sky"></span>
+          </div>
+          <div className="rightSide">
+            <div className="temperature celsius">12<span className="unit"><sup>o</sup>C</span></div>
+            <div className="location">Paris, France</div>
+            <div className="wind">
+              <span className="windDirection west"></span>
+              <span className="windSpeed">12 mph</span>
+            </div>
+          </div>
+          <ul className="forecast">
+            <li>
+              <span className="day">Monday</span>
+              <span className="cloud rainy"></span>
+              <span className="temperature">15<sup>o</sup></span>
+            </li>
+            <li>
+              <span className="day">Tuesday</span>
+              <span className="cloud sunny"></span>
+              <span className="temperature">16<sup>o</sup></span>
+            </li>
+            <li>
+              <span className="day">Wednesday</span>
+              <span className="cloud cloudy"></span>
+              <span className="temperature">13<sup>o</sup></span>
+            </li>
+            <li>
+              <span className="day">Thursday</span>
+              <span className="cloud rainy"></span>
+              <span className="temperature">12<sup>o</sup></span>
+            </li>
+            <li>
+              <span className="day">Friday</span>
+              <span className="cloud overrcast"></span>
+              <span className="temperature">18<sup>o</sup></span>
+            </li>
+          </ul>
+        </div>
       </div>
     );
+
+    //const {weather} = this.props;
   }
 }
 
